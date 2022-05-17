e9b5b48f58ee8ff2a65fcf24438dc856d043b700
sammiesleek <samlocomotive1@gmail.com>
2022-05-17 06:36:34 +0100

    .

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -1,5 +1,6 @@
 const drk = document.querySelector('.mode')
 const drks = document.querySelector('.tgb')
+const brnd = document.querySelectorAll('.brnd img')
 
 
 drk.addEventListener('click', ()=>{
@@ -9,11 +10,17 @@ drk.addEventListener('click', ()=>{
         drks.classList.remove('ri-sun-fill')
         drks.classList.add('ri-moon-fill')
         document.body.classList.add('dark-theme');
+        brnd.forEach(bran =>{
+            bran.src='asset/img/white-logo.png'
+        });
+      
     }else if(drks.classList.contains('ri-moon-fill') && document.body.classList.contains('dark-theme')) {
         drks.classList.remove('ri-moon-fill')
         drks.classList.add('ri-sun-fill')
         document.body.classList.remove('dark-theme');
-         
+          brnd.forEach(bran =>{
+            bran.src='asset/img/logo.png'
+        });
     }
 
     
@@ -34,13 +41,17 @@ drk.addEventListener('click', ()=>{
     if(localStorage.getItem("theme") == "light"){
             drks.classList.add('ri-sun-fill');
             drks.classList.remove('ri-moon-fill')
-            console.log("light")
            document.body.classList.remove('dark-theme');
+            brnd.forEach(bran =>{
+            bran.src='asset/img/logo.png'
+        });
     }else if(localStorage.getItem("theme") == "dark"){
            drks.classList.remove('ri-sun-fill');
            document.body.classList.add('dark-theme');
            drks.classList.add('ri-moon-fill')
-           console.log("dark")
+            brnd.forEach(bran =>{
+            bran.src='asset/img/white-logo.png'
+        });
     }else{
         localStorage.setItem("theme", "light")
     }
